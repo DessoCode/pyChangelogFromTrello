d049e7f963cc8aff27cc256171e552800a1a162e
Snipergen <46800074+Snipergen@users.noreply.github.com>
2019-02-28 22:03:37 +0100

    added tags option

--- pyExportTrelloList.py
+++ pyExportTrelloList.py
@@ -15,24 +15,28 @@ installed_packages = [r.decode().split('==')[0] for r in reqs.split()]
 #Open the json file where the api keys are stored.
 #check if file exists
 try:
-	with open('TrelloKeys.json', encoding="utf8") as f:
-		keys = json.load(f)
+        with open('TrelloKeys.json', encoding="utf8") as f:
+                keys = json.load(f)
 except FileNotFoundError:
-	print('TrelloKeys.json was not found.')
-	input('Press ENTER to exit')
+        print('TrelloKeys.json was not found.')
+        input('Press ENTER to exit')
 
 
 #get API keys out of the TrelloKeys.json
 api_key = keys['key']
 api_token = keys['token']
 api_boardId = keys['boardId']
+#card tags string
+global card_tags
+card_tags = ''
 
+#URL for the urrlib call
 boardURL = 'https://api.trello.com/1/boards/' + api_boardId + '?key=' + \
             api_key + '&token=' + api_token + '&fields=all&cards=all&card_fields=all&pluginData=true&card_pluginData=true&lists=all'
 try:
-	response = urllib.request.urlopen(boardURL)
+        response = urllib.request.urlopen(boardURL)
 except:
-	input("Could not access Trello API. Please check your keys in TrelloKeys.json...")
+        input("Could not access Trello API. Please check your keys in TrelloKeys.json...")
 jsonFile=response.read()
 data = json.loads(jsonFile)
 boardName = data['name']
@@ -52,13 +56,24 @@ for lists in data['lists']:
     #     sys.exit("ERROR: Done 13-02-2019COULD NOT FIND LIST....")
     #     break
 time.sleep(0.5)
-print('list id is:' + listId)
+print('list id is:' + listId + '\n')
 time.sleep(0.5)
+print("Do you want tags included? Y/N:")
+tags_allowed = input()
+
 toPasteTxt = listName + ':\n'
 print('-------------------The Changelog Is---------------------')
 for cards in data['cards']:
     if(listId == cards['idList']):
-        changeTxt = '* '+ cards['name']
+        if tags_allowed.lower() == 'y':
+            if not len(cards['labels']) == 0:
+                for tags in cards['labels']:
+                    card_tags += '[' + str(tags['name']) + '] '
+            else:
+                card_tags = ''
+
+        changeTxt = '* '+ card_tags + cards['name']
+        card_tags = ''
         toPasteTxt += changeTxt + '\n'
         print(changeTxt)
         time.sleep(0.1)
